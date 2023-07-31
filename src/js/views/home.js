694103d32e889960abb21c32b99bf34fc623c7bf
Argelio Baca <49691196+Argeliosk8@users.noreply.github.com>
2023-07-31 21:44:43 +0000

    created the contact view with static data"

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="d-flex justify-content-between" id="mainContainer">
+		<div className="container col-3" id='imgContainer'>
+			<img src="https://reqres.in/img/faces/1-image.jpg" class="img-thumbnail" alt="..."/>
+		</div>
+		<div className="d-flex flex-column align-items-start col-6" id='infoContainer'>
+			<h4>David Rocket</h4>
+			<p>address</p>
+			<p>email</p>
+			<p>phone</p>
+		</div>
+		<div className="container col-3" id='buttonsContainer'>
+			<h4>Buttons</h4>
+		</div>
 	</div>
 );
