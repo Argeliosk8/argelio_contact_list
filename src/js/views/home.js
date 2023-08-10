9f378cb60f93d01d321e8daddd3ce0da0c27357a
Argelio Baca <49691196+Argeliosk8@users.noreply.github.com>
2023-08-10 18:18:56 +0000

    fixed the useEffect, home page is now displaying the contacts on the agenda

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,29 +4,27 @@ import "../../styles/home.css";
 
 export function Home(){
 	const {store, actions} = useContext(Context)
-useContext(()=>{
-	actions.getContacts()
-}, [])
+	useEffect(()=>{actions.getContacts()}, [])
 
 	return (
-		<>
+		<div className="d-flex flex-column">
 			{ store.contacts.map((contact, id)=>(
-	<div className="d-flex justify-content-between" id="mainContainer" key={id}>
-		<div className="container col-3" id='imgContainer'>
-			<img src="https://reqres.in/img/faces/1-image.jpg" class="img-thumbnail" alt="..."/>
-		</div>
-		<div className="d-flex flex-column align-items-start col-6" id='infoContainer'>
-			<h4>{contact.full_name}</h4>
-			<p>{contact.address}</p>
-			<p>{contact.email}</p>
-			<p>{contact.phone}</p>
-		</div>
-		<div className="container col-3" id='buttonsContainer'>
-			<h4>Buttons</h4>
-		</div>
-	</div>
+				<div className="d-flex justify-content-between" id="mainContainer" >
+					<div className="container col-3" id='imgContainer'>
+						<img src="https://reqres.in/img/faces/1-image.jpg" class="img-thumbnail" alt="..."/>
+					</div>
+					<div className="d-flex flex-column align-items-start col-6" id='infoContainer'>
+						<h4>{contact.full_name}</h4>
+						<p>{contact.address}</p>
+						<p>{contact.email}</p>
+						<p>{contact.phone}</p>
+					</div>
+					<div className="container col-3" id='buttonsContainer'>
+						<h4>Buttons</h4>
+					</div>
+				</div>
 		))}
-		</>
+		</div>
 		
 	)
 }
